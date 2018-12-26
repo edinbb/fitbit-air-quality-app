e08788770877fada7d2ad64e5dfe0bd581668a96
Edin Bajrambasic <edin.bajrambasic@gmail.com>
2018-12-26 10:09:46 -0500

    removed async

--- companion/aqicn-api.js
+++ companion/aqicn-api.js
@@ -3,49 +3,54 @@ import { StationData } from "./station-data";
 
 export class AqicnAPI {
   constructor(token) {
-    this.baseUrl = "https://api.waqi.info/feed";
-    this.token = token;
+    this._baseUrl = "https://api.waqi.info/feed";
+    this._token = token;
+    this._retries = 8; //fetch() retry counter
+    this._retry = 0;
   }
 
-  async cityStation(uid) {
-    let json = await this.fetch("/@uid", uid);
-    return new StationData(json, null).map();
+  cityStation(uid) {
+    return new Promise((resolve, reject) => {
+      this._retry = 0;
+      this._fetch("/@uid", uid)
+        .then(json => resolve(new StationData(json, null).map()))
+        .catch(ex => reject(ex));
+    });
   }
 
-  async nearbyStation(loc) {
-    let json = await this.fetch("/geo:lat;lon", loc);
-    return new StationData(json, loc).map();
+  nearbyStation(loc) {
+    return new Promise((resolve, reject) => {
+      this._retry = 0;
+      this._fetch("/geo:lat;lon", loc)
+        .then(json => resolve(new StationData(json, loc).map()))
+        .catch(ex => reject(ex));
+    });
   }
 
-  async fetch(endUrl, params) {
-    let retries = 5; //fetch() retry counter
-    let url = this.baseUrl + endUrl;
+  _fetch(endUrl, params) {
+    let url = this._baseUrl + endUrl;
     for (const key in params) {
       if (params.hasOwnProperty(key)) {
         url = url.replace(key, params[key]);
       }
     }
-    url = url + "/?token=" + this.token;
-    try {
-      let error;
-      for (let i = 0; i < retries; i++) {
-        try {
-          console.log(`Fetching station data...`);
-          let response = await fetch(url);
-          let json = await response.json();
-          if (json.status === "error" || json.status === "nug")
-            throw new DataError("Data status prop is error or nug");
-          return json;
-        } catch (e) {
-          console.warn(`Temporary error - ${e}`);
-          error = e;
+    url = url + "/?token=" + this._token;
+    return fetch(url)
+      .then(resp => resp.json())
+      .then(json => {
+        if (json.status === "error" || json.status === "nug")
+          throw new DataError("Data status prop is error or nug");
+        return json;
+      }).catch(ex => {
+        if (this._retry < this._retries) {
+          console.warn(`Temporary error - ${ex}`);
+          this._retry++;
+          return this._fetch(endUrl, params);
+        } else {
+          console.error(`Permanent error - ${ex}`);
+          if (ex.type) throw ex;
+          throw new FetchError("Failed to fetch!");
         }
-      }
-      throw error;
-    } catch (e) {
-      console.error(`Permanent error - ${e}`);
-      if (e.type) throw e;
-      throw new FetchError("Failed to fetch!");
-    }
+      });
   }
 }
